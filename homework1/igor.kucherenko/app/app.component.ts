e5d07b79cb43b4ca77c4de0de58710f33f963ae0
Igor Kucherenko <hotncold1989@gmail.com>
2018-01-23 23:59:02 +0300

    Improve the logic adding structural directives

--- homework1/igor.kucherenko/app/app.component.ts
+++ homework1/igor.kucherenko/app/app.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
 
+interface IUser {
+  name: string;
+  surname: string;
+  position: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  users: IUser[] = [
+    {
+      name: 'Ivan',
+      surname: 'Petrov',
+      position: 'FE developer',
+    },
+    {
+      name: 'Sergey',
+      surname: 'Ivanov',
+      position: 'BE developer',
+    },
+    {
+      name: 'Maria',
+      surname: 'Sidorova',
+      position: 'QA Engineer',
+    },
+  ];
 }
