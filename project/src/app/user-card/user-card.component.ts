08d1b10b051e2f4411fe4e206e294338284f4a6d
Pavel <davidov.pavel@gmail.com>
2018-01-23 14:27:40 +0300

    prepare homework1

---
+++ project/src/app/user-card/user-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.css']
+})
+export class UserCardComponent implements OnInit {
+
+  @Input() user: User;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  randomColor(card) {
+    card.style.backgroundColor = this.getRandomColor();
+  }
+
+}
