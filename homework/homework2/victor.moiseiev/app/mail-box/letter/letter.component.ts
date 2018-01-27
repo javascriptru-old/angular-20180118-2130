ca1f1e352905aaec7a3c13ff522b1a2d19aef73d
Victor Moiseiev <vimo@247e.com>
2018-01-27 15:33:32 +0200

    Victor Moiseiev's homework 2

---
+++ homework/homework2/victor.moiseiev/app/mail-box/letter/letter.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Letter } from '../../Model/letter';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-letter',
+  templateUrl: 'letter.component.html',
+  styleUrls: ['letter.component.css']
+})
+export class LetterComponent implements OnInit, OnDestroy {
+
+  private initTime: Date;
+  @Input() private letter: Letter;
+  @Output() public selectToDelete: EventEmitter<number> = new EventEmitter<number>();
+  constructor() { }
+
+  ngOnInit() {
+    this.initTime = new Date();
+  }
+
+  ngOnDestroy(): void {
+
+  }
+  emitDeleting() {
+    this.selectToDelete.emit(this.letter.id);
+    console.log('lifetime: ' + ((new Date().getTime() - this.initTime.getTime()) / 1000) + ' seconds');
+  }
+}
