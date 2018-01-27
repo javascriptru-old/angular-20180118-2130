cb05bdd9ddb481d5af322a1a2c02656e07841166
DavidovPavel <davidov.pavel@gmail.com>
2018-01-27 23:33:07 +0300

    homework #2

---
+++ project/src/app/mail-box/mail-box.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MailInfo } from '../mail-info';
+
+@Component({
+  selector: 'app-mail-box',
+  template:
+    `<app-mail *ngFor="let mail of mails"
+    [mail]="mail"
+    (clearMail)="clearMail($event)"></app-mail>`
+})
+export class MailBoxComponent implements OnInit {
+
+  mails: MailInfo[] = [];
+  constructor() { }
+
+  ngOnInit() {
+    setInterval(this.newMail.bind(this), 3000);
+    setInterval(this.newMail.bind(this), 5000);
+  }
+
+  newMail() {
+    this.mails = this.mails
+      .concat(new MailInfo())
+      .sort((a, b) => b.received.valueOf() - a.received.valueOf());
+  }
+
+  clearMail(mail: MailInfo) {
+    this.mails = this.mails.filter(m => m !== mail);
+  }
+
+}
