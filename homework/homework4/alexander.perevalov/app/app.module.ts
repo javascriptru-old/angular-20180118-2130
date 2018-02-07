ab5fc6bcce5d5b7dd9bcdcc16d0112bb5e3fad04
aperevalov <a.perevalov@tradedealer.ru>
2018-02-08 00:11:01 +0500

    del

--- homework/homework4/alexander.perevalov/app/app.module.ts
+++ homework/homework4/alexander.perevalov/app/app.module.ts
@@ -1,44 +1,23 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AuthService } from './services/auth.service';
-import { LogService } from './services/log.service';
-import { MailService } from './services/mail.service';
-import { DashboardModule } from './dashboard/dashboard.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { MailBoxComponent } from './mail-box/mail-box.component';
-import { MailItemComponent } from './mail-box/mail-item/mail-item.component';
-import { UserService } from './services/user.service';
-import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UserRegisterComponent } from './user-register/user-register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MailBoxComponent,
-    MailItemComponent,
     UserRegisterComponent
   ],
   imports: [
     BrowserModule,
-    DashboardModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule
   ],
   providers: [
-    UserService,
-    MailService,
-    AuthService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LogService,
-      multi: true
-    }
   ],
   bootstrap: [AppComponent]
 })
--- homework/homework4/alexander.perevalov/app/mail-box/interfaces/mail-item.inteface.ts
+++
@@ -1,6 +0,0 @@
-export interface MailItem {
-    mailbox: string;
-    subject: string;
-    body:    string;
-    to:      string;
-}
