39a44fc890268bf44b2d4c491a346623e6b84669
DavidovPavel <davidov.pavel@gmail.com>
2018-02-11 18:53:18 +0300

    clear

--- homework/homework2/pavel.davidov/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<app-mail-box></app-mail-box>'
-})
-export class AppComponent { }
--- homework/homework2/pavel.davidov/app/mail-box/mail-info.ts
+++
@@ -1,6 +0,0 @@
-export class MailInfo {
-    received: Date;
-    constructor() {
-        this.received = new Date();
-     }
-}
